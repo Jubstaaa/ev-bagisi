7ce32b55983e7686d4453dcb36ca406fc1c80ada
Jubstaaa <ilkerbalcilartr@gmail.com>
2023-02-12 00:18:54 +0300

    info

--- src/App.js
+++ src/App.js
@@ -64,6 +64,24 @@ function App() {
         </>
       ) : (
         <>
+          <div className="alert alert-info shadow-lg mb-5">
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="stroke-current flex-shrink-0 w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                ></path>
+              </svg>
+              <span>Ayın Bağışcısını evin duvarına kaplama yaptırıyoruz.</span>
+            </div>
+          </div>
           <div className="flex flex-col justify-start items-center gap-5">
             <div className="flex flex-wrap xl:flex-nowrap justify-center sm:justify-between items-center w-full gap-5 md:gap-0 lg:gap-5">
               <div className="bg-base-100 rounded-box col-span-3 row-span-3 mx-2 grid w-72 flex-shrink-0 place-items-center items-center gap-4 p-4 py-8 shadow-xl xl:mx-0 xl:w-1/2 svelte-1n6ue57">
